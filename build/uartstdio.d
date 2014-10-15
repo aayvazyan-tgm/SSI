8c564d38e1baa330936ac8aa869c49da5caa0ecf
Ari Ayvazyan <ariay@live.at>
2014-10-15 19:18:18 +0200

    Initial commit

---
+++ build/uartstdio.d
@@ -0,0 +1,16 @@
+build/uartstdio.o: src/uartstdio.c \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ /opt/arm-none-eabi/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdarg.h \
+ /opt/tivaware/inc/hw_ints.h /opt/tivaware/inc/hw_memmap.h \
+ /opt/tivaware/inc/hw_types.h /opt/tivaware/inc/hw_uart.h \
+ /opt/tivaware/driverlib/debug.h /opt/tivaware/driverlib/interrupt.h \
+ /opt/tivaware/driverlib/rom.h /opt/tivaware/driverlib/rom_map.h \
+ /opt/tivaware/driverlib/sysctl.h /opt/tivaware/driverlib/uart.h \
+ /opt/tivaware/utils/uartstdio.h
